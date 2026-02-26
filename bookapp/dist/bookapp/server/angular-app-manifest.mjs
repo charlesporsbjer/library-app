91cd053b9b4411fffacbb1fca6d6628295693e60
Charles Porsbjer <charles.porsbjer@gmail.com>
2026-02-26 19:58:08 +0100

    Add theme variable to index.html

--- bookapp/dist/bookapp/server/angular-app-manifest.mjs
+++ bookapp/dist/bookapp/server/angular-app-manifest.mjs
@@ -42,8 +42,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5548, hash: '06c2eafb0581c2f306eaaeaeb9a49886083e5a2122e22eeb0a2eccee752f75df', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1056, hash: '00617ed8e278edeef48ecd44c402bae11d06bb5eb522b89b892f0e0a298700cf', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5778, hash: 'c3b3f38798e0a349e9217d4c5c6386b9055539cae2d492d5cbdc849c53b0b418', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1286, hash: '1ba5aa67d48c720a1f0e93f7c21c0cbffc3cab83ab9dd7997824054d8048bac8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-FQTL74HQ.css': {size: 309565, hash: 'RU+v73P503g', text: () => import('./assets-chunks/styles-FQTL74HQ_css.mjs').then(m => m.default)}
   },
 };
