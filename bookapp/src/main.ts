729452de7084e2f058aa75d6bf3827a964ffc694
Charles Porsbjer <charles.porsbjer@gmail.com>
2026-02-26 18:04:10 +0100

    Fix typo in angular.json

--- bookapp/src/main.ts
+++ bookapp/src/main.ts
@@ -2,5 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { provideClientHydration } from '@angular/platform-browser'
+
+bootstrapApplication(App, {
+  ...appConfig,
+providers: [
+    ...(appConfig.providers ?? []),
+    provideClientHydration()
+  ]
+})
+.catch((err) => console.error(err));
